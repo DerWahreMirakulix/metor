c18bd74260d3a7cbc7a4e38a145a0f9e1eb62c56
LuRex <lamplmair.julian@gmail.com>
2025-02-15 00:31:50 +0100

    update setup and license

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="metor",
     version="0.1",
-    packages=find_packages(), 
+    packages=find_packages(),
     install_requires=[
         "stem",
         "PySocks"
     ],
+    include_package_data=True,
+    package_data={
+        'metor': ['tor.exe'] 
+    },
     entry_points={
         "console_scripts": [
             "metor = metor.cli:main"
         ]
     },
-    author="DerWahreMirakulix",
+    author="LuRex",
     description="A simple Tor messenger with persistent onion address and chat commands.",
     license="MIT",
 )
